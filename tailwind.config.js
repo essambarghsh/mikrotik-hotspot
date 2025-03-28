b30431da043fe43f6f731175b3d88c9d9a6d9ac9
Essam Barghsh <em2002017@gmail.com>
2025-03-28 14:32:53 +0200

    Refactor stylesheets and update script paths for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.html",
-    "./support/**/*.html",
-    "login.html",
-    "alogin.html",
-    "header.html",
-    "footer.html",
-    "pos.html",
-    "app.js",
-    "loadComponents.js",
-  ],
-  theme: {
-    extend: {
-      textColor: {
-  			skin: {
-  				primary: 'var(--primary-color)',
-  				primary_dark: 'var(--primary-dark-color)',
-  			}
-  		},
-  		backgroundColor: {
-  			skin: {
-  				primary: 'var(--primary-color)',
-  				primary_dark: 'var(--primary-dark-color)',
-  			}
-  		},
-  		borderColor: {
-  			skin: {
-  				primary: 'var(--primary-color)',
-  				primary_dark: 'var(--primary-dark-color)',
-  			}
-  		},
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        sm: "100%",
-        md: "100%",
-        lg: "100%",
-        xl: "1240px",
-        "2xl": "1360px",
-      },
-    },
-  },
-  plugins: [],
-};
+    content: [
+      "./components/**/*.html",
+      "./support/**/*.html",
+      "login.html",
+      "alogin.html",
+      "header.html",
+      "footer.html",
+      "pos.html",
+      "app.js",
+      "loadComponents.js",
+    ],
+  };
