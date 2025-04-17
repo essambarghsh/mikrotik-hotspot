f7a62e551cc2b9dbfe3736fe05ab85c1a6cfbf73
Essam Barghsh <em2002017@gmail.com>
2025-04-17 15:41:44 +0200

    Downgrade Tailwind CSS from v4 to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./components/**/*.html",
-      "./support/**/*.html",
-      "login.html",
-      "alogin.html",
-      "header.html",
-      "footer.html",
-      "pos.html",
-      "app.js",
-      "loadComponents.js",
-    ],
-  };
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: [
+    "./components/**/*.html",
+    "./support/**/*.html",
+    "login.html",
+    "alogin.html",
+    "header.html",
+    "footer.html",
+    "pos.html",
+    "app.js",
+    "loadComponents.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
